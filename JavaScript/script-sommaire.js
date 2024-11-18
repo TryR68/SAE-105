f5f7549148d7a49a2de53f243bde1a89744e6a44
TryR68 <trystan.roussel88@gmail.com>
2024-11-18 23:01:36 +0100

    refonte du sommaire
    le js marche, le css est propre et la page est cohérante, juste modifier encore le css (hoover et couleurs)

--- JavaScript/script-sommaire.js
+++ JavaScript/script-sommaire.js
@@ -1,26 +1,18 @@
-let clées = Object.keys(SAE)
-
-console.log(clées)
-
-
 
 differentes_sae =''
-
-// for(let propriété in clées){
-//     differentes_sae += "<div><h2>" + clées[propriété] + "</h2></div>"  
-// }
-
-// document.querySelector("main").innerHTML = differentes_sae
 
 
 for (let key in SAE) {
     
     // Récupérer les informations de chaque SAE
+    let Num_SAE = key
     let titre = SAE[key].titre;
     let competences = SAE[key].compétences;
     let description = SAE[key].description;
-    differentes_sae += "<div><h2>" + titre + "</h2><p>" + competences+ "</p><p>" + description +"</p></div>"  
+    differentes_sae += "<div class='case'><h2>" + Num_SAE + "</h2><h3>" + titre + "</h3><p>" + description+ "</p><a href=''>"  + competences+ "</a></div>"
 
-  }
+
+}
+
   document.querySelector("main").innerHTML = differentes_sae
 
