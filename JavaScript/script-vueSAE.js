ecfa9a7dd0efbfeb4fab3b191254a08057aa7bef
TryR68 <trystan.roussel88@gmail.com>
2024-11-19 22:48:23 +0100

    fin du JS
    plus qu'a faire tout le css et le html et fin de la SAE

--- JavaScript/script-vueSAE.js
+++ JavaScript/script-vueSAE.js
@@ -1,16 +1,38 @@
 let param = new URLSearchParams(location.search);
 let num_SAE = param.get('num_SAE');
 
-let infos_sae = ''
+let infos_sae = '';
 
 let titre = SAE[num_SAE].titre;
 let competences = SAE[num_SAE].compétences;
 let description = SAE[num_SAE].description;
+let apprentissages_critique = SAE[num_SAE].AC;
+let ressources = SAE[num_SAE].ressources; 
 
 
-infos_sae += "<div class='case'><h2>" + num_SAE + "</h2><h3>" + titre + "</h3><p>" + description+ "</p>"  + competences+"</a></div>"
-
-console.log(infos_sae)
+infos_sae += "<div><h2>" + num_SAE + "</h2><h3>" + titre + "</h3><p>" + description+ "</p>"  + competences+"</a></div>"
 
 
+
+
+// Ajouter les apprentissages critiques (AC)
+infos_sae += "<h4>Apprentissages critiques :</h4><ul>";
+for (let num_ac in apprentissages_critique) {
+  infos_sae += "<li>" + apprentissages_critique[num_ac] + "</li>";
+}
+infos_sae += "</ul>";
+
+
+
+// Ajouter les ressources nécessaires
+infos_sae += "<h4>Ressources nécessaires :</h4><ul>";
+for (let num_ress in ressources) {
+  infos_sae += "<li>" + ressources[num_ress] + "</li>";
+}
+infos_sae += "</ul>";
+
+
+console.log(infos_sae);
+
+// Afficher l'HTML dans la page
 document.querySelector("main").innerHTML = infos_sae;
