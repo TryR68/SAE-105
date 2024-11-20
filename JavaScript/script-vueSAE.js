ebac468e9234028dea0a78c6e9b22aaae7ad1f26
TryR68 <trystan.roussel88@gmail.com>
2024-11-20 22:09:17 +0100

    fin officielle du JS
    fin du JS et refonte de la DA de l'index

--- JavaScript/script-vueSAE.js
+++ JavaScript/script-vueSAE.js
@@ -2,37 +2,43 @@ let param = new URLSearchParams(location.search);
 let num_SAE = param.get('num_SAE');
 
 let infos_sae = '';
+let description_sae = '';
+let apprentissages_sae = '';
+let ressources_sae ='';
+
+
 
 let titre = SAE[num_SAE].titre;
 let competences = SAE[num_SAE].compétences;
 let description = SAE[num_SAE].description;
+let semestre = SAE[num_SAE].semestre;
 let apprentissages_critique = SAE[num_SAE].AC;
 let ressources = SAE[num_SAE].ressources; 
 
 
-infos_sae += "<div><h2>" + num_SAE + "</h2><h3>" + titre + "</h3><p>" + description+ "</p>"  + competences+"</a></div>"
+infos_sae += "<div class = 'infos_sae'><h2>" + num_SAE + "</h2><h3>" + titre + "</h3><p>Semestre : " + semestre + "</p></div>";
+description_sae += "<div class = 'description_sae'><p>" + description + "</p><div class = competences>" + competences + "</div>";
 
 
 
-
-// Ajouter les apprentissages critiques (AC)
-infos_sae += "<h4>Apprentissages critiques :</h4><ul>";
+// Ajouter les apprentissages critiques
+apprentissages_sae += "<h3>Apprentissages critiques :</h>";
 for (let num_ac in apprentissages_critique) {
-  infos_sae += "<li>" + apprentissages_critique[num_ac] + "</li>";
+  apprentissages_sae += "<div class = apprentissages_sae>" + apprentissages_critique[num_ac] + "</div>";
 }
-infos_sae += "</ul>";
 
 
 
 // Ajouter les ressources nécessaires
-infos_sae += "<h4>Ressources nécessaires :</h4><ul>";
+ressources_sae += "<h3>Ressources</h3>";
 for (let num_ress in ressources) {
-  infos_sae += "<li>" + ressources[num_ress] + "</li>";
+  ressources_sae += "<div class = ressources_sae>" + ressources[num_ress] + "</div>";
 }
-infos_sae += "</ul>";
 
 
-console.log(infos_sae);
 
 // Afficher l'HTML dans la page
-document.querySelector("main").innerHTML = infos_sae;
+document.querySelector("#infoSAE").innerHTML = infos_sae;
+document.querySelector("#descriptionSAE").innerHTML = description_sae;
+document.querySelector("#apprentissagesSAE").innerHTML = apprentissages_sae;
+document.querySelector("#ressourcesSAE").innerHTML = ressources_sae;
